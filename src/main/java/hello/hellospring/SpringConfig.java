192d7e6cef22a75f19cb274448b00a455cf859bc
wxodi0 <teayang06@gmail.com>
2023-09-26 20:03:52 +0900

    Add Component & Around

--- src/main/java/hello/hellospring/SpringConfig.java
+++ src/main/java/hello/hellospring/SpringConfig.java
@@ -35,10 +35,10 @@ public class SpringConfig {
         return new MemberService(memberRepository);
     }
 
-    @Bean
-    public TimeTraceAop timeTraceAop() {
-        return new TimeTraceAop();
-    }
+//    @Bean
+//    public TimeTraceAop timeTraceAop() {
+//        return new TimeTraceAop();
+//    }
 
 
 //    @Bean
